f6fe62836dce8b139760f2f50a7c01c0336085e5
ANNE MARCHASSON <annemarchasson@yahoo.fr>
2023-03-28 12:19:01 +0200

    var in component

--- my-new-app/src/components/PokemonCard.jsx
+++ my-new-app/src/components/PokemonCard.jsx
@@ -1,11 +1,33 @@
+//variable array pokemon
+const pokemonList = [
+    {
+      name: "bulbasaur",
+      imgSrc: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+    },
+    {
+      name: "mew",
+    },
+  ];
+
+// function creation card pokemon
 function PokemonCard (){
-return <figure>
-  <img 
-    src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" 
-    alt="pokemon image" 
-   />
-   <figcaption>bulbasaur</figcaption>
+    
+//variable pokemon with first pokemonList
+const pokemon = pokemonList[1];
+
+console.log(pokemonList);
+return pokemon.imgSrc !== undefined ?
+<figure>
+    <img src={pokemon.imgSrc} alt={pokemon.name} />
+    <figcaption>{pokemon.name}</figcaption>
+</figure>
+:
+<figure>
+    <p>???</p>
+    <figcaption>{pokemon.name}</figcaption>
 </figure>
 }
 
+
+// export function PokemondCard pour app.jsx
 export default PokemonCard;
